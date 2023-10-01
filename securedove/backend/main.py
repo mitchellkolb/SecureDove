5ace6fbca835c1383d747a504d9126f65aeecb34
Mitchell-kolb <mkolb321@gmail.com>
2023-09-30 19:34:28 -0700

    Temp push so the login register and delete user can be used by others

--- securedove/backend/main.py
+++ securedove/backend/main.py
@@ -51,7 +51,7 @@ app.add_middleware(
 )
 
 try:
-    #Should really put line 50 into a secure file that doesn't go on github but until everyone can run this code i'll leave it in the file as is. Security wise this is bad procedure
+    #Should really put line 55 into a secure file that doesn't go on github but until everyone can run this code i'll leave it in the file as is. Security wise this is bad procedure
     conn = psycopg2.connect('postgres://wjjloedt:JXFCuJI7Di3CtZ-enIemY9J_m8VxNXpZ@berry.db.elephantsql.com/wjjloedt')
     print('Connection Success!')
     connectionsucceeded = True
@@ -66,60 +66,35 @@ cur = conn.cursor()
 def Default():
     return {"Default": "Test Data For SecureDove CptS 428"}
 
-#This is a endpoint that allow you to query the database at a certain index that is passed in at the frontend by the user in messages.js
-@app.get("/get_DB_info")
-def get_DB_info(message_id: int):
-    if message_id > 4:
-         rows = [('We dont have messages at that index right now. Try 1-4',)]
-         return {"data": rows}
-    else:
-        cur.execute(f"SELECT message_text FROM Messages WHERE message_id = {message_id};")
-        rows = cur.fetchall()
-        print(rows)
-        return {"data": rows}
 
-@app.post("/new_groupchat")
-async def new_groupchat(groupchat_name: str, created_by: int):
-    #Looping through Groupchats until we find the lowest available groupchat_id number
-    cur.execute(f"SELECT * FROM Groupchats")
-    rows = cur.fetchall()
-    i = 1
-    for row in rows:
-         if i == row[0]:
-            i = i + 1
-            continue
-         else:
-              break
-    groupchat_id = i
 
-    #Inserting into the database
-    cur.execute(f"INSERT INTO Groupchats (groupchat_id, groupchat_name, created_by) VALUES ({groupchat_id}, '{groupchat_name}', '{created_by}')")
-    conn.commit()
-    return {"success": True}
 
+# ---------------------   LOGIN / USER   ENDPOINTS -----------------------
+
+# Verifies the user and returns a unqiue value for the user so they can use the website
 @app.post("/login")
 async def login(user: UserLogin):
     print("user:",user.email,"pass:", user.password)
     # Grabbing the row that has the corresponding email 
     try:
         cur.execute(f"SELECT * FROM Users WHERE email = '{user.email}'")
+        rows = cur.fetchall()
     except:
         print("Email not found, click register to create a new account")
         return {"error": "Email not linked to an account. Please create one."}
-    rows = cur.fetchall()
-
-    # Checking if the password matches
-    if rows[0][3] == user.password:
-        # we can have a global variable that gets updated with this line below in order to keep track of who's logged in.
-        # then, when we need to load messages we can check who's logged in by checking that global variable containing the current user's id. If it's 0 then no one is logged in.
-        global CURRENT_USER
-        CURRENT_USER = rows[0][0] # update current logged in user to this user_id. We will reference this for deleting an user and showing groupchats.
-        print("CURRENT_USER", CURRENT_USER)
-        print("Correct Credentials.")
-        return {"message":"Login successful!"}
-    else:
-        print("Incorrect Credentials.")
-        return {"error":"Incorrect credentials."}
+     # Checking if the password matches
+    if rows != []:
+        if rows[0][2] == user.email and rows[0][3] == user.password:
+            # we can have a global variable that gets updated with this line below in order to keep track of who's logged in.
+            # then, when we need to load messages we can check who's logged in by checking that global variable containing the current user's id. If it's 0 then no one is logged in.
+            print(rows[0][0])
+            print("Correct Credentials.")
+            return {"User_id": rows[0][0]}
+        else:
+            print("Incorrect Password.")
+            return {"error":"Incorrect credentials."}
+    print("Email is incorrect or User doesn't Exist")
+    return {"error":"User doesn't Exist"}
 
 #Endpoint that adds a user to the Users table
 @app.post("/register")
@@ -143,51 +118,32 @@ async def register(user: UserRegister):
     print("Register success.")
     return {"message": "Register successful!"}
 
-# endpoint to reset CURRENT_USER when user logs out
-@app.post("/logout")
-async def logout():
-    global CURRENT_USER
-    CURRENT_USER = 0
-    print("CURRENT USER:", CURRENT_USER)
-    return {"message":"Logout successful!"}
+#Deletes user based on user_id passed by the frontend by a logged in user
+@app.delete("/delete_user")
+async def delete_user(user_id: int):
+    try:
+        cur.execute(f"DELETE FROM users WHERE user_id = {user_id};")
+        conn.commit()
+        return {"Successfully deleted user with id=": user_id}
+    except Exception as e:
+        raise HTTPException(status_code=496, detail=str(e))
 
-#Endpoint that deletes a user from the Users table
-# @app.delete("/delete_user")
-# def delete_user(username: str):
-#     cur.execute(f"SELECT username FROM Users")
-#     rows = cur.fetchall()
-#     if(username in rows):
-#         # Delete user based on username input
-#         cur.execute(f"DELETE FROM Users WHERE username = '{username}';")
-#         conn.commit()
-#         return {"accountDeletion":"Success"}
-#     return {"accountDeletion": "Failed"}
 
-#Deletes user based on CURRENT_USER global variable, then resets it to 0
-@app.post("/delete_user")
-async def delete_user():
-    global CURRENT_USER
-    # print("inside delete")
-    print("CURRENT USER",CURRENT_USER)
-    if (CURRENT_USER!=0):
-        try:
-            # print("before query")
-            cur.execute(f'DELETE FROM Users WHERE user_id = {CURRENT_USER};')
-            conn.commit()
-            # print("after query")
-        except:
-            print("Error deleting user.")
-            return{"error": "Error accessing database."}
-        print("Successfully deleted user with id=", CURRENT_USER)
-        CURRENT_USER = 0
-        return {"message": "Deletion successful!"}
-    else:
-        print("No user is currently logged in.")
-        return {"error": "No user is currently logged in."}
-    
-# Invite User
-@app.post("/invite_user")
-def invite_user(user_id):
+
+
+# ---------------------   INVITATION ENDPOINTS -----------------------
+
+# View list of invitations for that user
+@app.get("/view_invite")
+def view_invite(user_id: int):
+    cur.execute(f"SELECT username FROM Users WHERE user_id = {user_id}")
+    rows = cur.fetchall()
+    print(rows)
+    return {"Error" : "Ok"}
+
+# Create new invitation to chat from one user to another
+@app.post("/new_invite")
+def new_invite(user_id: int):
     # Checks if current user_id matches Groupchats.created by'
     cur.execute(f"SELECT * FROM Groupchats where groupchat_id = {CURRENT_USER}")
     rows = cur.fetchall()
@@ -203,35 +159,16 @@ def invite_user(user_id):
     else:
         print("User is not authorized to invite users")
 
-# Exit groupchat
-@app.post("/exit_groupchat")
-def exit_groupchat():
-    # Queries user_id in UserGroups
-    cur.execute(f"SELECT user_id FROM UserGroups")
+# Decide, Reject or Accept for an existing invite
+@app.get("/decide_invite")
+def decide_invite(user_id: int, decision: str):
+    cur.execute(f"SELECT username FROM Users WHERE user_id = {user_id}")
     rows = cur.fetchall()
-    if(CURRENT_USER in rows):
-        cur.execute(f"DELETE FROM UserGroups WHERE user_id = {CURRENT_USER}")
-        conn.commit()
-        return {"exitChat" : "Success"}
-    return {"exitChat" : "Failed"}
+    print(rows)
+    return {"Error" : "Ok"}
 
-# Accepts user to join usergroup
-@app.post("/join_groupchat")
-def join_groupchat():
-    # Checks current users' user_id
-    cur.execute(f"SELECT user_id FROM Users")
-    rows = cur.fetchall()
-    if(CURRENT_USER in rows):
-        cur.execute(f"INSERT INTO UserGroups WHERE user_id = {CURRENT_USER}")
-        conn.commit()
-        return {"acceptInvite" : "Success"}
-    return {"exitChat" : "Failed"}
 
-# Return to print the reject message
-def reject_invite():
-    print("PRINT TO UI: Invitation Rejected")
 
-#
 # Gives a snapshot of the chats for the side bar of the logged in user. It gives the other user that is in the chat and the last message sent with its timestamp
 # Returns all chats for that user based off of user_id. Each chat returned will have the chat_id, Other username, last message sent, last message timestamp
 @app.get("/left_bar_chat")
@@ -241,8 +178,8 @@ def left_bar_chat(user_id: int):
     rows = cur.fetchall()
     if rows != []:
         username = rows[0][0]
-        print(username)
         try:
+            #This query matches the chat, user, messages tables and returns all the chats that the user is apart of with both usernames of each chat and the chat_id with the last message in each chat and the timestamp of that message
             cur.execute(f"""
                 SELECT 
                     c.chat_id, 
@@ -277,10 +214,7 @@ def left_bar_chat(user_id: int):
                     c.user1_id = {user_id} OR c.user2_id = {user_id};
             """)
             rows = cur.fetchall()
-            print(rows)
             fixedrows = [tuple(ele for ele in sub if ele != username) for sub in rows]
-            print("okay")
-            print(fixedrows)
             columns = ["Chat_id", "Other User", "Last Message", "Timestamp"]
             data = [dict(zip(columns, row)) for row in fixedrows]
             return {"left_chat_data": data}
@@ -292,12 +226,13 @@ def left_bar_chat(user_id: int):
             
 
 
+# ---------------------   CHATS / MESSAGES   ENDPOINTS -----------------------
+
 # Loads the chat that is requested and returns username, message, timestamp of each message sent in ORDERED by the time they where sent so from top to bottom is the earilest to latest messages
 @app.get("/load_chat")
 def load_chat(chat_id: int):
     cur.execute(f"""SELECT chat_id FROM chats WHERE chat_id = {chat_id}""")
     rows = cur.fetchall()
-    print(rows)
     if rows != []:
         try:
             cur.execute(f"""
